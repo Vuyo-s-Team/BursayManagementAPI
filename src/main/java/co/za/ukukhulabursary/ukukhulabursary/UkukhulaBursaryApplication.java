8f26860de28a5080b58d3ac1f74f358da91170f6
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-02-16 14:47:39 +0200

    initial commit

---
+++ src/main/java/co/za/ukukhulabursary/ukukhulabursary/UkukhulaBursaryApplication.java
@@ -0,0 +1,13 @@
+package co.za.ukukhulabursary.ukukhulabursary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkukhulaBursaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkukhulaBursaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/za/ukukhulabursary/ukukhulabursary/UkukhulabursaryApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.ukukhulabursary.ukukhulabursary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkukhulabursaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
