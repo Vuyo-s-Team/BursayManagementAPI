1ead09814200a6f075cfbfda6930f7258cb93fc5
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-02-18 11:24:14 +0200

    added data to document model

--- src/main/java/co/za/ukukhulabursary/ukukhulabursary/model/Document.java
+++ src/main/java/co/za/ukukhulabursary/ukukhulabursary/model/Document.java
@@ -1,5 +1,13 @@
 package co.za.ukukhulabursary.ukukhulabursary.model;
 
+import lombok.Data;
+
+@Data
 public class Document {
+   
+    private Long DocumentID;
+    private String Transcript;
+    private String IdentityDocument;
+    private Integer ApplicationID;
     
 }
