3d77abbb230af216ec072d044c60d9f57a4594b6
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-02-18 10:56:00 +0200

    added findall method

---
+++ src/main/java/co/za/ukukhulabursary/ukukhulabursary/repository/IDocumentRepository.java
@@ -0,0 +1,11 @@
+package co.za.ukukhulabursary.ukukhulabursary.repository;
+import co.za.ukukhulabursary.ukukhulabursary.model.Document;
+
+import java.util.List;
+
+
+public interface IDocumentRepository {
+
+    List<Document> findAll();
+
+}
