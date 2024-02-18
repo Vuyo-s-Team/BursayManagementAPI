fc24df34f35049f65dc7616e4afd163bcb7a6465
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-02-18 11:27:47 +0200

    added findall method

--- src/main/java/co/za/ukukhulabursary/ukukhulabursary/repository/implementation/DocumentRepository.java
+++ src/main/java/co/za/ukukhulabursary/ukukhulabursary/repository/implementation/DocumentRepository.java
@@ -1,5 +1,27 @@
 package co.za.ukukhulabursary.ukukhulabursary.repository.implementation;
 
-public class DocumentRepository {
-    
+import co.za.ukukhulabursary.ukukhulabursary.mapper.DocumentMapper;
+import co.za.ukukhulabursary.ukukhulabursary.model.Document;
+import co.za.ukukhulabursary.ukukhulabursary.repository.IDocumentRepository;
+
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@AllArgsConstructor
+public class DocumentRepository implements  IDocumentRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final DocumentMapper mapper;
+
+    @Override
+    public List<Document> findAll() {
+        String sql = "SELECT * FROM [dbo].[Province]";
+        return jdbcTemplate.query(sql, mapper);
+    }
+
 }
