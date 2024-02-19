0098893d6327ff95a08593a57dc7fcba6c1c0a23
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-02-19 11:20:59 +0200

    create file for StudentApplication interface

---
+++ src/main/java/co/za/ukukhulabursary/ukukhulabursary/repository/IStudentApplicationRepository.java
@@ -0,0 +1,18 @@
+package co.za.ukukhulabursary.ukukhulabursary.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import co.za.ukukhulabursary.ukukhulabursary.model.Document;
+
+public interface IStudentApplicationRepository {
+     List<Document> getAllStudentApplication();
+
+    Optional<Document> getStudentApplicationById(long DocumentID);
+    
+    Document saveStudentApplication(Document document);
+
+    Optional<Document> updateStudentApplication(long documentID, Document updatedDocument);
+
+    
+}
